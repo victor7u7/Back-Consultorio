cdf45de5d3f356800c2a20be522b692b35f99774
Yair <yairmasterlol@gmail.com>
2023-07-02 19:55:35 -0600

    2023-07-02-07:55:35

--- consultorioBack/settings.py
+++ consultorioBack/settings.py
@@ -101,10 +101,22 @@ WSGI_APPLICATION = "consultorioBack.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "g6Z51RUqcGiGnDdjRm3g",
+        "HOST": "containers-us-west-187.railway.app",
+        "PORT": "6189",
     }
 }
 
