0184b6a7a5c504f48f1f5bff1a1a3d257898869c
Yair <yairmasterlol@gmail.com>
2023-07-09 19:59:05 -0600

    2023-07-09-07:59:05

--- src/components/Url.jsx
+++ src/components/Url.jsx
@@ -1,2 +1,2 @@
 // export const api = "http://127.0.0.1:8000";
-export const api = "https://consultorio-production.up.railway.app";
+export const api = "https://back-consultorio-production.up.railway.app";
